f210fe9e6addbe2503cd545d6e809decf87a6938
Efraim82 <163622615+Efraim82@users.noreply.github.com>
2024-04-13 12:26:24 +0800

    ProductList

---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <div v-for="product in products" :key="product.id">
+        <ProductItem :product="product" @delete="deleteProduct(product.id)" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import ProductItem from "./ProductItem.vue";
+  import { getAllProducts, deleteProduct } from "../data/products.js";
+  
+  export default {
+    components: {
+      ProductItem,
+    },
+    data() {
+      return {
+        products: getAllProducts(),
+      };
+    },
+    methods: {
+      deleteProduct(id) {
+        deleteProduct(id);
+        this.products = getAllProducts();
+      },
+    },
+  };
+  </script>
